8ab665ec2135ae26ad0af1cb27d9c6d157776a9c
Alp Emre Elmas <elmasalpemre@gmail.com>
2024-08-23 23:01:54 +0200

    fix: fix db query (missing data)

--- src/server/rest.js
+++ src/server/rest.js
@@ -10,7 +10,7 @@ export const serveRest = async (data) => {
     app.use(cors())
 
     app.get("/devices", async (_, res) => {
-        const devicesWithAllData = await connection.raw("select d.id, d.name as device_name, u.admin as is_admin, u.email, (fv.major || '.' || fv.minor || '.' || fv.patch) as version, u.last_update from devices d join users u on u.email = d.user_email JOIN firmware_versions fv ON d.firmware_version_id = fv.id JOIN ( SELECT u.device_id, MAX(u.finished) AS last_update FROM updates u GROUP BY device_id) u ON d.id = u.device_id where u.subscription_ends >= current_timestamp")
+        const devicesWithAllData = await connection.raw("select d.id, d.name as device_name, u.admin as is_admin, u.email, (fv.major || '.' || fv.minor || '.' || fv.patch) as version, upt.last_update from devices d join users u on u.email = d.user_email join firmware_versions fv on d.firmware_version_id = fv.id left join ( select upt.device_id, MAX(upt.finished) as last_update from updates upt group by upt.device_id) upt on d.id = upt.device_id where u.subscription_ends >= current_timestamp")
         const lastFirmwareVersion = await connection.raw("select (firmware_versions.major || '.' || firmware_versions.minor || '.' || firmware_versions.patch) as latest_version from firmware_versions order by id desc limit 1")
         res.send({deviceWithAllData: devicesWithAllData, lastFirmwareVersion: lastFirmwareVersion[0].latest_version});
     });
