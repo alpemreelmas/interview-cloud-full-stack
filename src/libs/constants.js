55cd3bbc6cfbec0d4b6abdc36b60276c10fed907
Alp Emre Elmas <elmasalpemre@gmail.com>
2024-08-23 22:46:28 +0200

    feat: add last update to the table with detailed sortable fields, add status column without labelled.

--- src/libs/constants.js
+++ src/libs/constants.js
@@ -3,5 +3,6 @@ export const API_URL = "http://localhost:4000";
 export const displayColumnToQueryColumn = {
     "user": "email",
     "name": "device_name",
-    "firmware": "firmware_version",
+    "version": "version",
+    "updated": "last_update",
 }
